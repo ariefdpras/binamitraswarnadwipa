a5b50c3b688057847ed81b928626c7311b0a4794
ariefdpras <ariefprasetyo1994@gmail.com>
2024-09-17 16:00:00 +0700

    update web bms

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: { primary: "#f59e0b", secondary: "#475569", dark: "#0f172a" },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
