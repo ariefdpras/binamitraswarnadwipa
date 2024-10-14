4b28961b1956b8bdabe4990e8a736de034255d12
ariefdpras <ariefprasetyo1994@gmail.com>
2024-10-14 15:23:51 +0700

    add blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "blog_1.html"],
   darkMode: "class",
   theme: {
     container: {
